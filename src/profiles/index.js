5be5ae9ef7e00142675b73cc17fb8a81277639f1
Peter Sefton <pt@ptsefton.com>
2023-08-16 12:50:11 +1000

    Removed notebook profile as it is not ready yet

--- src/profiles/index.js
+++ src/profiles/index.js
@@ -5,8 +5,9 @@
 import base from './base-profile.json'
 import schema from './schema.json'
 import language from './text-commons-collection-profile.json'
-import notebook from './ldaca-notebook-profile.json'
+//Not ready yet
+//import notebook from './ldaca-notebook-profile.json'
 
 
 
-export const profiles = [base, schema, language, notebook];
+export const profiles = [base, schema, language];
