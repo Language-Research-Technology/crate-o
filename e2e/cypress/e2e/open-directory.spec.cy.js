5f20767e1baa362299069cf0357109dc283b05b6
Moises Sacal <moisbo@gmail.com>
2023-08-24 12:14:02 +1000

    added cypress testing

--- e2e/cypress/e2e/open-directory.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('open directory spec', () => {
-  it('successfully loads', () => {
-    cy.visit('/');
-    cy.contains('Crate-O')
-    cy.contains("File").click();
-    cy.contains('Open Directory ').click();
-  })
-});
