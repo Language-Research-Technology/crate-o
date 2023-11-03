8d900f7694478f72a5c46176c7482420f74243d0
Moises Sacal <moisbo@gmail.com>
2023-11-03 11:54:50 +1100

    added mime

--- src/components/MediaPreview.vue
+++ src/components/MediaPreview.vue
@@ -1,18 +1,24 @@
 <script setup>
 import {watch, reactive} from "vue";
+import mime from 'mime/lite';
 
 const props = defineProps(['file']);
 const data = reactive({
-  contents: ''
+  contents: '',
 });
 
 function getURL() {
   return URL.createObjectURL(props.file)
 }
 
+function isText(file) {
+  const fileType = file.type || mime.getType(file.name);
+  return fileType.startsWith('text/') || fileType.match('json');
+}
+
 watch(() => props.file, async (newVal, oldVal) => {
   // data.activeLayout = state.meta[newVal['@id']].__activeLayout ??= 'About';
-  if (props.file && props.file.type.startsWith('text/')) {
+  if (props.file && isText(props.file)) {
     data.contents = await props.file.text();
   }
 }, {immediate: true});
@@ -35,11 +41,14 @@ watch(() => props.file, async (newVal, oldVal) => {
     <div class="p-4" v-else-if="file.type.startsWith('image/')">
       <img height="500px" :src="getURL()"/>
     </div>
-    <div v-else-if="file.type.startsWith('text/')">
+    <div v-else-if="isText(file)">
       <div class="whitespace-pre-wrap max-h-[36rem] overflow-y-auto">{{ data.contents }}</div>
     </div>
     <div class="w-full" v-else-if="file.type.startsWith('application/pdf')">
       <iframe class="w-full h-[66rem] overflow-y-auto" style="width: 100%" :src="getURL()"/>
+    </div>
+    <div v-else>
+      Cannot preview, unsupported type
     </div>
   </template>
   <template v-else>
