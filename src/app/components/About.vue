4bf3b629dfa8ee74cb7c599ea6a30e5cd4ebfeee
Otis Carmichael <ocarm0809@gmail.com>
2024-06-05 13:40:00 +1000

    Generate about page functionality added

--- src/app/components/About.vue
+++ src/app/components/About.vue
@@ -1,9 +1,62 @@
 <script setup>
+import { marked } from 'marked';
+
+fetch('./README.md')
+      .then(response => response.text())
+      .then(markdown => {
+        const htmlContent = marked(slice_anchors(markdown));
+        document.getElementById("htmlContent").innerHTML = htmlContent;
+      })
+      .catch(error => console.error('Error fetching the Markdown file:', error))
+
+// Finds indexes of substrings in string
+function indexes(substring, string){
+  var a=[],i=-1;
+  while((i=string.indexOf(substring,i+1)) >= 0) {
+    substring.includes("Start") ? a.push([i + 14]) : a.push(i);
+  }
+  return a;
+}
+
+function slice_anchors(markdown) {
+  var starts = indexes("<!---Start--->", markdown);
+  var ends = indexes("<!---End--->", markdown);
+  var md_sliced = "";
+  try {
+    for (let i = 0; i < starts.length; i++) {
+      console.log("md slice from ", starts[i], " to ", ends[i], ": ", markdown.slice(starts[i], ends[i]))
+      md_sliced += markdown.slice(starts[i], ends[i]);
+    }
+    return md_sliced;
+  } catch(err) {
+    console.log(err);
+  }
+}
 
 //TODO: Insert code that will load the readme markdown into here as html
 </script>
+<style>
+  h1 {
+    @apply mb-3 text-xl md:text-xl
+  }
+  h2 {
+    @apply mb-3 text-lg md:text-xl
+  }
+  div {
+    @apply grid gap-2 text-gray-500 dark:text-gray-400
+  }
+  a {
+    @apply text-blue-600 dark:text-blue-500 hover:underline
+  }
+  ul {
+    @apply list-disc list-inside
+  }
+</style>
 <template>
+  <div id="htmlContent"></div>
   <div class="grid gap-2 text-gray-500 dark:text-gray-400">
+    
+
     <h2 class="mb-3 text-lg md:text-xl">About Crate-O</h2>
     <p class="">
       Crate-O is a browser-based editor for <a class="text-blue-600 dark:text-blue-500 hover:underline"
