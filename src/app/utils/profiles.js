3582070913c8c84c15b2abaf86638585ad7bdb97
rosanna-smith <rosanna.smith@uq.edu.au>
2024-10-15 11:02:27 +1100

    added comprehensive ldac mode

--- src/app/utils/profiles.js
+++ src/app/utils/profiles.js
@@ -1,6 +1,7 @@
 const profileUrls = [
     'https://language-research-technology.github.io/ro-crate-modes/modes/default.json',
     'https://language-research-technology.github.io/ro-crate-modes/modes/language-data-commons-collection.json',
+    'https://language-research-technology.github.io/ro-crate-modes/modes/comprehensive-ldac.json',
     'https://language-research-technology.github.io/ro-crate-modes/modes/schema.json',
     'https://language-research-technology.github.io/ro-crate-modes/modes/software.json',
     'https://language-research-technology.github.io/ro-crate-modes/modes/base.json',
