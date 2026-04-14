b40682e35f8127129fe8f108556d74944d7ce4e8
moisbo <moisbo@gmail.com>
2026-04-14 01:05:45 +0000

    Deploying to gh-pages from @ Language-Research-Technology/crate-o@81a8deaead7921ae36cec65905aab3ab149cc5fc 🚀

---
+++ assets/TestSchema-BlIumDqg.js
@@ -0,0 +1,1 @@
+const n={__name:"TestSchema",setup(a){var t;return async function(){const s=await fetch("https://schema.org/version/latest/schemaorg-current-https.jsonld");s.status===200&&(t=await s.json())}(),(e,s)=>null}};export{n as default};
